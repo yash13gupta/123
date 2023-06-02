6dba0942d41aa72c07793d19948fa829942a2eb5
Yash gupta <134619016+yash13gupta@users.noreply.github.com>
2023-06-02 05:48:01 -0700

    Add files via upload

---
+++ resume.java.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+class normal{
+    public static void main(String[] args) {   
+        Scanner yash = new Scanner(System.in);
+        System.out.println("        job seeker details");
+
+        System.out.println("           -------------   ");
+
+        System.out.println("enter your obj");
+        String object = yash.next();
+        System.out.println("enter your name");
+        String name = yash.next();
+        System.out.println("enter your addres");
+        String addres = yash.next();
+        System.out.println("enter your email");
+        String email = yash.next();
+        System.out.println("enter your phone number");
+        int phone_number = yash.nextInt();
+        System.out.println("enter your education");
+        String education = yash.next();
+        System.out.println("enter your skils");
+        String skills = yash.next();
+        System.out.println(" enter your experience");
+        String experience = yash.next();
+        System.out.println("enter your interests");
+        String interest = yash.next();
+        System.out.println("enter additional information");
+        String addi_information = yash.next("enter your language");
+        String language = yash.next();
+        System.out.println("enter your activities");
+        String activities = yash.next();
+
+     System.out.println("----------------");
+
+    System.out.println("Object ||->          "      + object);
+    System.out.println("Name   ||->         "       + name);
+    System.out.println("Addres ||->           "     + addres);
+    System.out.println("Email  ||->          "      + email);
+    System.out.println("Phone number ||->     "     + phone_number);
+    System.out.println("Education  ||->       "     + education);
+    System.out.println("Skills     ||->     "       + skills);
+    System.out.println("Experience ||->      "      + experience);
+    System.out.println("interests  ||->      "      + interest);
+    System.out.println("Additional Information ||-> " + addi_information);
+    System.out.println("Language    ||->     "       + language);
+    System.out.println("Activities  ||->     "       + activities); 
+    
+
+
+    }
+
+    }
+        
+       
+
+
+    
+
+
+
+
+
+
+
+
+
+         
+
+  
